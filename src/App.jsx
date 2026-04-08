7121d438419ff268cdd3f1d588337ede8ebfac7c
LukaJovanovic357 <jovanovicluka276@outlook.com>
2026-04-08 18:27:32 +0200

    setup folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
     return (
         <>
-            <h1 className='text-yellow-700'>My React app</h1>
-            <h2>second line</h2>
+            <h1 className=''>My React app</h1>
         </>
     );
 }
