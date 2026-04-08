5d27df41fb9ca01d92e941d062c23c2594e56292
LukaJovanovic357 <jovanovicluka276@outlook.com>
2026-04-08 18:10:53 +0200

    app initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <h1 className='text-yellow-700'>My React app</h1>
+            <h2>second line</h2>
+        </>
+    );
 }
 
 export default App;
